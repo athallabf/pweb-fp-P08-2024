205d643a449784e86758a7d2f7def4f61490f847
nayyarashila <nayyarashila26@gmail.com>
2024-12-25 15:40:12 +0700

    feat: add rules

--- frontend/pages/rules.vue
+++ frontend/pages/rules.vue
@@ -4,17 +4,17 @@
       class="max-w-7xl mx-auto bg-white rounded-lg shadow-xl overflow-hidden"
     >
       <header
-        class="bg-[#789DBC] text-white py-6 px-4 sm:px-6 lg:px-8 text-center"
+        class="bg-[#dfaea1] text-white py-6 px-4 sm:px-6 lg:px-8 text-center"
       >
         <h1 class="text-4xl font-extrabold">Aturan Kost</h1>
-        <p class="mt-2 text-white font-light">
+        <p class="mt-2 text-[#fffcf5]">
           Harap diperhatikan dan dipatuhi untuk kenyamanan bersama.
         </p>
       </header>
 
       <div class="p-6 sm:p-8 lg:p-10 space-y-8">
         <section>
-          <h2 class="text-2xl font-semibold text-red-300 flex items-center">
+          <h2 class="text-2xl font-semibold text-[#895159] flex items-center">
             <AlertTriangle class="mr-2 h-6 w-6" />
             Peringatan
           </h2>
@@ -22,22 +22,22 @@
             <div
               v-for="(item, index) in warnings"
               :key="index"
-              class="bg-[#C9E9D2] bg-opacity-30 p-4 rounded-lg flex items-start"
+              class="bg-[#eef4ce] bg-opacity-30 p-4 rounded-lg flex items-start"
             >
               <component
                 :is="item.icon"
-                class="h-6 w-6 text-[#789DBC] mr-3 mt-1 flex-shrink-0"
+                class="h-6 w-6 text-[#374375] mr-3 mt-1 flex-shrink-0"
               />
               <div>
-                <h3 class="font-bold text-[#789DBC]">{{ item.title }}</h3>
-                <p class="mt-1 text-sm text-gray-600">{{ item.content }}</p>
+                <h3 class="font-bold text-[#374375]">{{ item.title }}</h3>
+                <p class="mt-1 text-sm text-[#4d5c60]">{{ item.content }}</p>
               </div>
             </div>
           </div>
         </section>
 
         <section>
-          <h2 class="text-2xl font-semibold text-red-300 flex items-center">
+          <h2 class="text-2xl font-semibold text-[#895159] flex items-center">
             <Book class="mr-2 h-6 w-6" />
             Peraturan
           </h2>
@@ -45,14 +45,14 @@
             <div
               v-for="(item, index) in rules"
               :key="index"
-              class="bg-white border border-[#C9E9D2] p-4 rounded-lg shadow-sm flex items-start"
+              class="bg-[white border] border-[#374375] p-6 rounded-lg shadow-sm flex items-start"
             >
               <component
                 :is="item.icon"
-                class="h-6 w-6 text-[#789DBC] mr-3 mt-1 flex-shrink-0"
+                class="h-6 w-6 text-[#374375] mr-3 mt-1 flex-shrink-0"
               />
               <div>
-                <h3 class="font-bold text-[#789DBC]">{{ item.title }}</h3>
+                <h3 class="font-bold text-[#374375]">{{ item.title }}</h3>
                 <p class="mt-1 text-sm text-gray-600">{{ item.content }}</p>
               </div>
             </div>
@@ -61,7 +61,7 @@
       </div>
 
       <footer
-        class="bg-[#FEF9F2] text-center py-4 text-sm text-[#789DBC] font-light"
+        class="bg-[#eef4ce] text-center py-4 text-sm text-[#374375] font-light"
       >
         Terima kasih atas kerja sama Anda dalam mematuhi aturan kost ini.
       </footer>
@@ -88,24 +88,24 @@ const warnings = [
     icon: Trash2,
     title: "Kebersihan",
     content:
-      "Jaga kebersihan kamar dan area bersama. Buang sampah pada tempatnya.",
+      "Wajin menjaga kebersihan kamar dan fasilitas bersama. Buang sampah pada tempatnya.",
   },
   {
     icon: Music,
-    title: "Kebisingan",
+    title: "Keributan",
     content:
-      "Hindari membuat kebisingan yang mengganggu, terutama pada malam hari.",
+      "Dilarang membuat keributan yang dapat meganggu penghuni kos yang lain.",
   },
   {
     icon: Home,
     title: "Keamanan",
-    content: "Pastikan pintu kamar dan pintu utama selalu terkunci.",
+    content: "Diharapkan untuk mengunci pintu kamar masing-masing. Pintu pagar menggunakan sidik jari penghuni kos.",
   },
   {
     icon: Users,
     title: "Tamu",
     content:
-      "Tamu hanya diperbolehkan berkunjung pada jam yang telah ditentukan.",
+      "Tamu diperbolehkan menginap tanpa dipungut biaya. ",
   },
 ];
 
@@ -113,12 +113,12 @@ const rules = [
   {
     icon: Clock,
     title: "Jam Malam",
-    content: "Penghuni harus kembali ke kost sebelum jam 22.00.",
+    content: "Pintu pagar akan dikunci setelah jam 23:00.",
   },
   {
     icon: CreditCard,
     title: "Pembayaran",
-    content: "Pembayaran sewa kost harus dilakukan tepat waktu setiap bulan.",
+    content: "Pembayaran kos dilakukan pada tanggal 1-10 setiap bulannya.",
   },
   {
     icon: AlertTriangle,
@@ -129,22 +129,22 @@ const rules = [
   {
     icon: PawPrint,
     title: "Hewan Peliharaan",
-    content: "Hewan peliharaan tidak diperbolehkan kecuali dengan izin khusus.",
+    content: "Hewan peliharaan tidak diperbolehkan masuk kecuali dengan izin khusus.",
   },
   {
     icon: Home,
-    title: "Perbaikan",
-    content: "Penghuni tidak diperbolehkan melakukan perbaikan tanpa izin.",
+    title: "Fasilitas",
+    content: "Penghuni dilarang menambah atau merusak fasilitas kos yang sudah ada.",
   },
   {
     icon: Power,
-    title: "Penggunaan Listrik dan Air",
-    content: "Gunakan listrik dan air dengan bijak.",
+    title: "Listrik & Air",
+    content: "Penghuni kos akan membeli token listrik secara mandiri. Biaya air ditanggung oleh kos.",
   },
   {
     icon: Users,
-    title: "Kegiatan Sosial",
-    content: "Kegiatan sosial harus mendapatkan izin terlebih dahulu.",
+    title: "Acara",
+    content: "Tidak diperbolehkan melakukan sebuah acara di dalam area kos kecuali sudah memiliki izin.",
   },
 ];
 </script>
